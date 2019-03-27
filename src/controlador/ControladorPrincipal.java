e5d7a65a4031f6a60173fe7ff1fb181f79b5d6b9
Rubén Pruñanosa <rubenprunyanosa@gmail.com>
2019-03-27 21:03:21 +0100

    Menu + Controlador Principal H E C H O

--- src/controlador/ControladorPrincipal.java
+++ src/controlador/ControladorPrincipal.java
@@ -2,7 +2,10 @@ package controlador;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
 import javax.swing.JOptionPane;
+
 import model.Companyia;
 import persistencia.GestorPersistencia;
 import vista.MenuPrincipal;
@@ -13,116 +16,136 @@ import vista.MenuPrincipal;
  */
 public class ControladorPrincipal implements ActionListener {
 
-    private static MenuPrincipal menuPrincipal;
-    private static final String FITXER = "companyia.xml";
-    private static final int MAXCOMPANYIES = 3;
-    private static Companyia[] companyies = new Companyia[MAXCOMPANYIES];
-    private static int posicioCompanyies = 0;
-    private static Companyia companyiaActual = null;
-    private static int tipusElement = 0;
-    static private GestorPersistencia gp = new GestorPersistencia();
-    static private final String[] METODEPERSISTENCIA = {"XML", "JDBC"};
+	private static MenuPrincipal menuPrincipal;
+	private static final String FITXER = "companyia.xml";
+	private static final int MAXCOMPANYIES = 3;
+	private static Companyia[] companyies = new Companyia[MAXCOMPANYIES];
+	private static int posicioCompanyies = 0;
+	private static Companyia companyiaActual = null;
+	private static int tipusElement = 0;
+	static private GestorPersistencia gp = new GestorPersistencia();
+	static private final String[] METODEPERSISTENCIA = { "XML", "JDBC" };
 
-    /*  
-    CONSTRUCTOR
-    Paràmetres:cap
-    Accions:
-    - S'inicialitza l'atribut menuPrincipal (això mostrarà el menú principal)
-    - A cada botó del menú principal se li afegeix el listener tenint en compte què el
-    mètode actionPerformed, està implementat en aquesta classe.
-     */
-    public ControladorPrincipal() {
+	public static final String M_SORTIR = "0. Sortir";
+	public static final String M_COMPANYIA = "1. Menú Companyia";
+	public static final String M_AVIO = "2. Menu Avió";
 
-    }
+	/*
+	 * CONSTRUCTOR Paràmetres:cap Accions: - S'inicialitza l'atribut menuPrincipal
+	 * (això mostrarà el menú principal) - A cada botó del menú principal se li
+	 * afegeix el listener tenint en compte què el mètode actionPerformed, està
+	 * implementat en aquesta classe.
+	 */
+	public ControladorPrincipal() {
+		menuPrincipal = new MenuPrincipal();
+		addActionListeners(menuPrincipal.getMenuButtons());
 
-    /*  
-    Paràmetres: ActionEvent    
-    Acció: S'ha de cridar a seleccionarOpcio segons l'opció premuda. Penseu que l'opció es correspon amb
-    la posició que el botó ocupa a l'array de botons de menuPrincipal.        
-    Retorn: cap
-     */
-    public void actionPerformed(ActionEvent e) {
+	}
 
-    }
+	private void addActionListeners(JButton[] menuPrButtons) {
+		for (JButton jButton : menuPrButtons) {
+			jButton.addActionListener(this);
+		}
+	}
 
-    private void seleccionarOpcio(int opcio) {
-        switch (opcio) {
-            case 0: //Sortir
-                System.exit(0);
-                break;
-            case 1: //Gestió companyia
-                menuPrincipal.getFrame().setVisible(false);
-                ControladorCompanyia controladorCompanyia = new ControladorCompanyia();
-                break;
-            case 2: //Gestió avió
-                if (companyiaActual != null) {
-                    menuPrincipal.getFrame().setVisible(false);
-                    ControladorAvio controladorAvio = new ControladorAvio();
-                } else {
-                    JOptionPane.showMessageDialog(null, "Has de seleccionar una companyia al menu companyia.", "Avís", JOptionPane.PLAIN_MESSAGE);
-                }
-                break;
-        }
-    }
+	/*
+	 * Paràmetres: ActionEvent Acció: S'ha de cridar a seleccionarOpcio segons
+	 * l'opció premuda. Penseu que l'opció es correspon amb la posició que el botó
+	 * ocupa a l'array de botons de menuPrincipal. Retorn: cap
+	 */
+	public void actionPerformed(ActionEvent e) {
+		switch (e.getActionCommand()) {
+		case M_SORTIR:
+			seleccionarOpcio(0);
+			break;
+		case M_COMPANYIA:
+			seleccionarOpcio(1);
+			break;
+		case M_AVIO:
+			seleccionarOpcio(2);
+			break;
+		}
+	}
 
-    public static String getFITXER() {
-        return FITXER;
-    }
+	private void seleccionarOpcio(int opcio) {
+		switch (opcio) {
+		case 0: // Sortir
+			System.exit(0);
+			break;
+		case 1: // Gestió companyia
+			menuPrincipal.getFrame().setVisible(false);
+			ControladorCompanyia controladorCompanyia = new ControladorCompanyia();
+			break;
+		case 2: // Gestió avió
+			if (companyiaActual != null) {
+				menuPrincipal.getFrame().setVisible(false);
+				ControladorAvio controladorAvio = new ControladorAvio();
+			} else {
+				JOptionPane.showMessageDialog(null, "Has de seleccionar una companyia al menu companyia.", "Avís",
+						JOptionPane.PLAIN_MESSAGE);
+			}
+			break;
+		}
+	}
 
-    public static int getMAXCOMPANYIES() {
-        return MAXCOMPANYIES;
-    }
+	public static String getFITXER() {
+		return FITXER;
+	}
 
-    public static String[] getMETODEPERSISTENCIA() {
-        return METODEPERSISTENCIA;
-    }
+	public static int getMAXCOMPANYIES() {
+		return MAXCOMPANYIES;
+	}
 
-    public static MenuPrincipal getMenuPrincipal() {
-        return menuPrincipal;
-    }
+	public static String[] getMETODEPERSISTENCIA() {
+		return METODEPERSISTENCIA;
+	}
 
-    public static void setMenuPrincipal(MenuPrincipal menuPrincipal) {
-        ControladorPrincipal.menuPrincipal = menuPrincipal;
-    }
+	public static MenuPrincipal getMenuPrincipal() {
+		return menuPrincipal;
+	}
 
-    public static Companyia[] getCompanyies() {
-        return companyies;
-    }
+	public static void setMenuPrincipal(MenuPrincipal menuPrincipal) {
+		ControladorPrincipal.menuPrincipal = menuPrincipal;
+	}
 
-    public static void setCompanyies(Companyia[] companyies) {
-        ControladorPrincipal.companyies = companyies;
-    }
+	public static Companyia[] getCompanyies() {
+		return companyies;
+	}
 
-    public static int getPosicioCompanyies() {
-        return posicioCompanyies;
-    }
+	public static void setCompanyies(Companyia[] companyies) {
+		ControladorPrincipal.companyies = companyies;
+	}
 
-    public static void setPosicioCompanyies(int posicioCompanyies) {
-        ControladorPrincipal.posicioCompanyies = posicioCompanyies;
-    }
+	public static int getPosicioCompanyies() {
+		return posicioCompanyies;
+	}
 
-    public static Companyia getCompanyiaActual() {
-        return companyiaActual;
-    }
+	public static void setPosicioCompanyies(int posicioCompanyies) {
+		ControladorPrincipal.posicioCompanyies = posicioCompanyies;
+	}
 
-    public static void setCompanyiaActual(Companyia companyiaActual) {
-        ControladorPrincipal.companyiaActual = companyiaActual;
-    }
+	public static Companyia getCompanyiaActual() {
+		return companyiaActual;
+	}
 
-    public static int getTipusElement() {
-        return tipusElement;
-    }
+	public static void setCompanyiaActual(Companyia companyiaActual) {
+		ControladorPrincipal.companyiaActual = companyiaActual;
+	}
 
-    public static void setTipusElement(int tipusElement) {
-        ControladorPrincipal.tipusElement = tipusElement;
-    }
+	public static int getTipusElement() {
+		return tipusElement;
+	}
 
-    public static GestorPersistencia getGp() {
-        return gp;
-    }
+	public static void setTipusElement(int tipusElement) {
+		ControladorPrincipal.tipusElement = tipusElement;
+	}
 
-    public static void setGp(GestorPersistencia gp) {
-        ControladorPrincipal.gp = gp;
-    }
+	public static GestorPersistencia getGp() {
+		return gp;
+	}
+
+	public static void setGp(GestorPersistencia gp) {
+		ControladorPrincipal.gp = gp;
+	}
 
 }
--- src/vista/MenuPrincipal.java
+++ src/vista/MenuPrincipal.java
@@ -1,5 +1,7 @@
 package vista;
 
+import java.awt.GridLayout;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 
@@ -8,46 +10,62 @@ import javax.swing.JFrame;
  * @author root
  */
 public class MenuPrincipal {
-    private JFrame frame;
+	private JFrame frame;
 
-    private JButton[] menuButtons = new JButton[3];
+	private JButton[] menuButtons = new JButton[3];
 
-    private final int AMPLADA = 800;
-    private final int ALCADA = 600;
+	private final int AMPLADA = 800;
+	private final int ALCADA = 600;
 
-    /* 
-    CONSTRUCTOR
-    Paràmetres:cap
-    Accions:
-    Heu d'inicialitzar els atributs d'aquesta classe fent el següent (no afegiu cap listener a cap control):
-            
-     - Heu d'inicialitzar l'objecte JFrame amb títol "Menú Principal" i layout Grid d'una columna
-     - Heu de crear els botons del formulari. Cada botó serà un element de l'array menuBotons amb les següents etiquetes:
-                        "0. Sortir"
-                        "1. Menú Companyia"
-                        "2. Menú Avio"
-     - Heu d'afegir-ho tot a l'atribut frame
-     - Heu de fer visible el frame amb l'amplada i alçada que proposen els atributs amplada i alcada
-     - Heu de fer que la finestra es tanqui quan l'usuari ho fa amb el control "X" de la finestra
-        
-    */ 
-    public MenuPrincipal() {
-        
-    }
+	/*
+	 * CONSTRUCTOR Paràmetres:cap Accions: Heu d'inicialitzar els atributs d'aquesta
+	 * classe fent el següent (no afegiu cap listener a cap control):
+	 * 
+	 * - Heu d'inicialitzar l'objecte JFrame amb títol "Menú Principal" i layout
+	 * Grid d'una columna - Heu de crear els botons del formulari. Cada botó serà un
+	 * element de l'array menuBotons amb les següents etiquetes: "0. Sortir"
+	 * "1. Menú Companyia" "2. Menú Avio" - Heu d'afegir-ho tot a l'atribut frame -
+	 * Heu de fer visible el frame amb l'amplada i alçada que proposen els atributs
+	 * amplada i alcada - Heu de fer que la finestra es tanqui quan l'usuari ho fa
+	 * amb el control "X" de la finestra
+	 * 
+	 */
+	public MenuPrincipal() {
+		// Construir frame
+		frame = new JFrame("Menú Principal");
+		frame.setSize(AMPLADA, ALCADA);
+		frame.setLayout(new GridLayout(3, 1));
+		frame.setResizable(false);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		addButtonsToFrame(frame); // Añadir botonera
+		// Visualizar después de construcción
+		frame.setVisible(true);
 
-    public JFrame getFrame() {
-        return frame;
-    }
+	}
 
-    public void setFrame(JFrame frame) {
-        this.frame = frame;
-    }
+	private void addButtonsToFrame(JFrame frame) {
+		//Configurar y añadir botones
+		menuButtons[0] = new JButton("0. Sortir");
+		menuButtons[1] = new JButton("1. Menú Companyia");
+		menuButtons[2] = new JButton("2. Menu Avió");
+		for (JButton jButton : menuButtons) {
+			frame.add(jButton);
+		}
+	}
 
-    public JButton[] getMenuButtons() {
-        return menuButtons;
-    }
+	public JFrame getFrame() {
+		return frame;
+	}
 
-    public void setMenuButtons(JButton[] menuButtons) {
-        this.menuButtons = menuButtons;
-    }
+	public void setFrame(JFrame frame) {
+		this.frame = frame;
+	}
+
+	public JButton[] getMenuButtons() {
+		return menuButtons;
+	}
+
+	public void setMenuButtons(JButton[] menuButtons) {
+		this.menuButtons = menuButtons;
+	}
 }
